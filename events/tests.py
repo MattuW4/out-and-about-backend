a33c675f63b20dfa54eda61f02dcf4075b62c013
Matt Waters <enjoi_waters@hotmail.com>
2024-03-01 18:48:32 +0000

    Create event detail tests to check whether event can be retrieved by id, event cant be retrived by invalid id, user can update own event and user cant update other users event

--- events/tests.py
+++ events/tests.py
@@ -10,11 +10,11 @@ class EventListViewTests(APITestCase):
     Tests for the Event model list view
     """
     def setUp(self):
-        User.objects.create_user(username='adam', password='pass')
+        User.objects.create_user(username='matt', password='passw')
         
     def test_can_list_events(self):
         
-        adam = User.objects.get(username='adam')
+        adam = User.objects.get(username='matt')
         Event.objects.create(owner=adam, title='a title', event_date='2024-12-12')
         response = self.client.get('/events/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -22,7 +22,7 @@ class EventListViewTests(APITestCase):
         print(len(response.data))
         
     def test_logged_in_user_can_create_event(self):
-        self.client.login(username='adam', password='pass')
+        self.client.login(username='matt', password='passw')
         response = self.client.post('/events/', {'title': 'a title', 'event_date': '2024-12-12'})
         count = Event.objects.count()
         self.assertEqual(count, 1)
@@ -33,3 +33,43 @@ class EventListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         count = Event.objects.count()
         self.assertEqual(count, 0)
+        
+class EventDetailViewTests(APITestCase):
+    def setUp(self):
+        matt = User.objects.create_user(username='matt', password='passw')
+        kaitlin = User.objects.create_user(username='kaitlin', password='passw')
+        Event.objects.create(
+            owner=matt, 
+            title='a title', 
+            description='matts content',
+            event_date='2024-12-12'
+        )
+        Event.objects.create(
+            owner=kaitlin, 
+            title='some other title', 
+            description='kaitlins content',
+            event_date='2024-12-12'
+        )
+        
+    def test_can_retrieve_event_using_valid_id(self):
+        response = self.client.get('/events/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_cant_retrieve_event_using_invalid_id(self):
+        response = self.client.get('/events/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_event(self):
+        self.client.login(username='matt', password='passw')
+        response = self.client.put('/events/1/', {'title': 'a new title', 'event_date': '2024-12-12'})
+        event = Event.objects.filter(pk=1).first()
+        self.assertEqual(event.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_user_cant_update_someone_elses_event(self):
+        self.client.login(username='bobby', password='passw')
+        response = self.client.put('/events/2/', {'title': 'an edited title', 'event_date': '2024-12-12'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
