e12e2ad7105782a9b6b2a4b71c941755e3369723
Matt Waters <enjoi_waters@hotmail.com>
2024-03-01 15:46:21 +0000

    Add image filter and image dimension validation to serializer.py

--- events/serializers.py
+++ events/serializers.py
@@ -10,6 +10,21 @@ class EventSerializer(serializers.ModelSerializer):
         source='owner.profile.image.url'
     )
     
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 *2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+        )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+        )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+        )
+        return value
+ 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -20,4 +35,5 @@ class EventSerializer(serializers.ModelSerializer):
             'id', 'owner', 'created_at', 'profile_id', 'updated_at',
             'title', 'event_date', 'profile_image', 'description',
             'category', 'image', 'is_owner','event_date', 'category',
+            'image_filter',
         ]
