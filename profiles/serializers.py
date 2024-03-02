2da78224844084c789568c5ad8e895943c63c78c
Matt Waters <enjoi_waters@hotmail.com>
2024-03-02 17:43:21 +0000

    Add filter to ProfileList and ProfileDetail

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from subscribers.models import Subscriber
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
@@ -8,6 +9,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     subscribing_id = serializers.SerializerMethodField()
+    events_count = serializers.ReadOnlyField()
+    subscribers_count = serializers.ReadOnlyField()
+    subscribing_count = serializers.ReadOnlyField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,7 +21,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.is_authenticated:
             subscribing = Subscriber.objects.filter(
-                owner=user, followed=obj.owner
+                owner=user, subscribed=obj.owner
             ).first()
             return subscribing.id if subscribing else None
         return None
@@ -26,4 +30,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 
             'content', 'image', 'is_owner', 'subscribing_id',
+            'events_count', 'subscribers_count', 'subscribing_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from oaa_api.permissions import IsOwnerOrReadOnly
@@ -8,15 +9,33 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        subscribers_count=Count('owner__subscribed', distinct=True),
+        subscribing_count=Count('owner__subscribing', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [    
+        'events_count',
+        'subscribers_count',
+        'subscribing_count',
+        'owner__subscribing__created_at',
+        'owner__subscribed__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a profile if you're the owner.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        subscribers_count=Count('owner__subscribed', distinct=True),
+        subscribing_count=Count('owner__subscribing', distinct=True)
+    ).order_by('-created_at')
     permission_classes = [IsOwnerOrReadOnly]
     
     
