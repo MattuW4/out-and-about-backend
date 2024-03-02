30b2de1c1066ce8dee8ba1acc120cfeb1d97d9fb
Matt Waters <enjoi_waters@hotmail.com>
2024-03-02 21:59:07 +0000

    Update allowed hosts and client origin dev in settings.py

--- oaa_api/settings.py
+++ oaa_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -64,11 +65,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 
-                 '8000-mattuw4-outandaboutback-wemrfgif75z.ws-eu108.gitpod.io',
-                 'oaa-app.herokuapp.com',
-                 '*',
-                ]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    '8000-mattuw4-outandaboutback-wemrfgif75z.ws-eu108.gitpod.io',
+    '*',
+    ]
 
 
 # Application definition
@@ -116,9 +118,10 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
