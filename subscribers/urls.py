347a3e2e79a4a93e3ef8acee6625c008312f1212
Matt Waters <enjoi_waters@hotmail.com>
2024-03-02 15:05:57 +0000

    Refactor event and profile view methods with generic views. Remove white space from subscribers url

--- subscribers/urls.py
+++ subscribers/urls.py
@@ -3,7 +3,7 @@ from subscribers import views
 
 urlpatterns = [
     path('subscribers/', views.SubsciberList.as_view()),
-    path('subscribers/ < int:pk > /', views.SubscriberDetail.as_view())
+    path('subscribers/<int:pk>/', views.SubscriberDetail.as_view())
 ]
 
 
