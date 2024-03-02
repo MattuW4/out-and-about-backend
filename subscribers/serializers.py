375a15452c508441aa72dc6fc22257f74a641442
Matt Waters <enjoi_waters@hotmail.com>
2024-03-02 14:27:18 +0000

    Create Subscriber serializer

---
+++ subscribers/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Subscriber
+from django.db import IntegrityError
+
+class SubscriberSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Subscriber model
+    Create method handles the unique constraint on 'owner' and 'subscribed'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    subscribed_name = serializers.ReadOnlyField(source='subscribed.username')
+
+    class Meta:
+        model = Subscriber
+        fields = [
+            'id', 'owner', 'subscribed_name', 'subscribed', 'created_at'
+        ]
+        
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You are already subscribing to this user!'
+            })
