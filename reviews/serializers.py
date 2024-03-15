dd0bba22c9c394527e845c77b264e85db79dee7b
Matt Waters <enjoi_waters@hotmail.com>
2024-03-15 15:10:10 +0000

    Add serializer.py and create serializers for review and review details

---
+++ reviews/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Review
+from django.db import IntegrityError
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Review model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Review
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'event',
+            'review', 'rating', 'is_owner', 'profile_id',
+            'profile_image',
+        ]
+
+    def create(self, validated_data):
+        """
+        Validation to stop a user reviewing the same event twice.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You have reviewed this event already!'
+            })
+
+
+class ReviewDetailSerializer(ReviewSerializer):
+    """
+    Serializer for the Review model used in Detail view
+    event is a read only field so that we dont have to set it on each update
+    """
+    event = serializers.ReadOnlyField(source='event.id')
