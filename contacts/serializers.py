7b00a0fae9e47cad67d03fc696276eb7b1dd092d
Matt Waters <enjoi_waters@hotmail.com>
2024-03-19 14:54:53 +0000

    Create contact app, model, view and serializer

---
+++ contacts/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    Contact model serializer
+
+    """
+    owner = serializers.ReadOnlyField(source="owner.username")
+    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_created_at(self, obj):
+        """
+        Returns time since the contact was created
+        """
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        """
+        Returns time since contact was updated
+        """
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Contact
+        fields = [
+            "id",
+            "owner",
+            "reason",
+            "content",
+            "profile_id",
+            "profile_image",
+            "created_at",
+            "updated_at",
+        ]
--- oaa_api/settings.py
+++ oaa_api/settings.py
@@ -101,6 +101,7 @@ INSTALLED_APPS = [
     'attending',
     'subscribers',
     'reviews',
+    'contacts',
 ]
 
 SITE_ID = 1
